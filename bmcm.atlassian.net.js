cb5ae54fc349a4c33c6c7e71b28e27dc9877f36c
Nick Babenko <nickbabenko@me.com>
2019-07-10 18:16:24 +0100

    Updated colours and styling

--- bmcm.atlassian.net.js
+++ bmcm.atlassian.net.js
@@ -3,16 +3,20 @@ window.addEventListener('load', () => {
         return
     }
 
-    let colours = document.querySelectorAll('.customfield_10017')
-    const colourMap = [
-        yellow: '',
-        blue: ''
-    ]
+    const colours = document.querySelectorAll('.customfield_10017')
+    const colourMap = {
+        yellow: '#F7B500',
+        blue: '#0091FF',
+        red: '#E02020',
+        green: '#6DD400'
+    }
     colours.forEach((colour) => {
         let colourCode = typeof colourMap[colour.innerText] === 'string'
             ? colourMap[colour.innerText]
             : colour.innerText;
-        colour.innerHTML = '<span style="width: 10px; height: 10px; background: ' + colourCode + '; display: inline-block;"></span>';
+        colour.innerHTML = '<span style="width: 24px; height: 24px; background: ' + colourCode + '; border-radius: 12px; display: inline-block;">' +
+                '<span style="width: 22px; height: 22px; display: inline-block; border-radius: 12px; border: 1px solid rgba(0, 0, 0, 0.18);"></span>'
+            '</span>';
     })
-    document.querySelector('.headerrow-customfield_10017 span').innerText = 'Status'
+    document.querySelector('.headerrow-customfield_10017 span').innerText = 'Indicator'
 });
