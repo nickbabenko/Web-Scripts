b7087075e2966dfb442717f178136fa4c96532f0
Nick Babenko <nickbabenko@me.com>
2019-07-10 20:14:24 +0100

    Removed load event

--- bmcm.atlassian.net.js
+++ bmcm.atlassian.net.js
@@ -1,22 +1,20 @@
-window.addEventListener('load', () => {
-    if (window.location.pathname !== '/issues/' && window.location.search !== '?filter=10007') {
-        return
-    }
+if (window.location.pathname !== '/issues/' && window.location.search !== '?filter=10007') {
+    return
+}
 
-    const colours = document.querySelectorAll('.customfield_10017')
-    const colourMap = {
-        yellow: '#F7B500',
-        blue: '#0091FF',
-        red: '#E02020',
-        green: '#6DD400'
-    }
-    colours.forEach((colour) => {
-        let colourCode = typeof colourMap[colour.innerText] === 'string'
-            ? colourMap[colour.innerText]
-            : colour.innerText;
-        colour.innerHTML = '<span style="width: 24px; height: 24px; background: ' + colourCode + '; border-radius: 12px; display: inline-block;">' +
-                '<span style="width: 22px; height: 22px; display: inline-block; border-radius: 12px; border: 1px solid rgba(0, 0, 0, 0.18);"></span>'
-            '</span>';
-    })
-    document.querySelector('.headerrow-customfield_10017 span').innerText = 'Risk'
-});
+const colours = document.querySelectorAll('.customfield_10017')
+const colourMap = {
+    yellow: '#F7B500',
+    blue: '#0091FF',
+    red: '#E02020',
+    green: '#6DD400'
+}
+colours.forEach((colour) => {
+    let colourCode = typeof colourMap[colour.innerText] === 'string'
+        ? colourMap[colour.innerText]
+        : colour.innerText;
+    colour.innerHTML = '<span style="width: 24px; height: 24px; background: ' + colourCode + '; border-radius: 12px; display: inline-block;">' +
+            '<span style="width: 22px; height: 22px; display: inline-block; border-radius: 12px; border: 1px solid rgba(0, 0, 0, 0.18);"></span>'
+        '</span>';
+})
+document.querySelector('.headerrow-customfield_10017 span').innerText = 'Risk'
